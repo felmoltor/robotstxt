2739393d396c25c66bb17a2c33ff7ae5ea548146
Felipe Molina <felmoltor@gmail.com>
2018-12-25 18:34:22 +0100

    Add confirmation for overwrite results and timeouts

--- robotstxt.sh
+++ robotstxt.sh
@@ -6,16 +6,32 @@ then
     exit 1
 fi
 
-echo "===== ROBOTS TXT ===="
+hits=$(whereis -b timeout | cut -f2 -d: | tr -d '\n' | wc -c)
+if [ $hits -eq 0 ];then
+    echo "You don't have the coreutils binary 'timeout'. Install it before executing this script"
+    exit 2
+fi
+
+echo "===== ROBOTS TXT Dictionary generator ===="
 DOMAINS=$1
+TIMEOUT=20
 
 if [ ! -d output ];then
     mkdir output
 fi
-echo "" > output/robots.entries.txt
-echo "" > output/robots.sorted.full.path.txt
-echo "" > output/robots.sorted.rootfolder.txt
-echo "" > output/robots.sorted.files.txt
+if [ -f output/robots.entries.txt ];then
+    read -p "Warning: output folder is not empty, do you want to delete the file contents of this folder [y/N]: " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "" > output/robots.entries.txt
+        echo "" > output/robots.sorted.full.path.txt
+        echo "" > output/robots.sorted.rootfolder.txt
+        echo "" > output/robots.sorted.files.txt
+    else
+        echo "User canceled the script"
+        exit 3
+    fi
+fi
 
 if [ -f $DOMAINS ];
 then
@@ -33,7 +49,12 @@ then
         fi
        
         echo "($cont/$ndom) Fetching robots from '$domain'..."
-        curl -L -s "$domain/robots.txt" | grep "Disallow:" | cut -d' ' -f2 >> output/robots.entries.txt
+        timeout $TIMEOUT bash -c "curl -L -s \"$domain/robots.txt\" | grep 'Disallow:' | cut -d' ' -f2 >> output/robots.entries.txt"
+        err=$?
+        if [ $err -gt 0 ];then
+            echo "The last command timed out after $TIMEOUT seconds. Skiping to the next domain"
+            continue
+        fi
     done
 
     echo ""
